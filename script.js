05c99c102191a1a4d033d275ae8cc35f74752781
chareeselam <chareeselam@gmail.com>
2026-03-25 17:25:15 -0400

    eric demo js

---
+++ script.js
@@ -0,0 +1,12 @@
+let data = [
+    {
+        name: 'G&T',
+        ingredients: [
+            'gin', 'tonic water', 'ice', 'lime'
+        ],
+        criteria:[
+            'happiness': 9,
+            'weather': 'sun',
+        ]
+    }
+]
